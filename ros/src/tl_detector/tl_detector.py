500e3e03666cd630c6f1651bb717fa85011b3e7a
gdangelo <gregory@gdangelo.fr>
2018-10-08 15:20:06 -0400

    feat: Use traffic light classifier in ROS detector node

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -44,7 +44,7 @@ class TLDetector(object):
         self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
 
         self.bridge = CvBridge()
-        self.light_classifier = TLClassifier()
+        self.light_classifier = TLClassifier('../../../models/ssd_sim/frozen_inference_graph.pb')
         self.listener = tf.TransformListener()
 
         self.state = TrafficLight.UNKNOWN
@@ -122,18 +122,16 @@ class TLDetector(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-
-        return light.state
-
-        # TODO: Implement classifier
-        '''if(not self.has_image):
-            self.prev_light_loc = None
+        if(not self.has_image):
+            #self.prev_light_loc = None
             return False
 
         cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
 
-        #Get classification
-        return self.light_classifier.get_classification(cv_image)'''
+        # Run traffic light classifier on current image
+        output = self.light_classifier.get_classification(cv_image)
+        rospy.logwarn(output)
+        return False
 
     def process_traffic_lights(self):
         """Finds closest visible traffic light, if one exists, and determines its
