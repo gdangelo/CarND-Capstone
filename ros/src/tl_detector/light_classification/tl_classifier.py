df5bd5114e34d35e2e0763ec36ce34d1c8ce2d84
gdangelo <gregory@gdangelo.fr>
2018-10-10 17:03:45 -0400

    refact: Load graph and tensors during node initialization

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -2,27 +2,55 @@ from styx_msgs.msg import TrafficLight
 import tensorflow as tf
 import numpy as np
 
+FROZEN_GRAPH_PATH = './models/ssd_sim/frozen_inference_graph.pb'
+
 class TLClassifier(object):
-    def __init__(self, model_file, min_score_thresh=.5):
+    def __init__(self):
         # Load a (frozen) Tensorflow model into memory
-        self.detection_graph = tf.Graph()
-        with self.detection_graph.as_default():
-            od_graph_def = tf.GraphDef()
-            with tf.gfile.GFile(model_file, 'rb') as fid:
-                serialized_graph = fid.read()
-                od_graph_def.ParseFromString(serialized_graph)
-                tf.import_graph_def(od_graph_def, name='')
+        self.detection_graph, self.all_tensor_names = self.load_graph()
+
+        # Load tensors needed for inference
+        self.num_detections = self.load_tensor('num_detections')
+        self.boxes = self.load_tensor('detection_boxes')
+        self.scores = self.load_tensor('detection_scores')
+        self.classes = self.load_tensor('detection_classes')
+        self.image_tensor = self.load_tensor('image_tensor')
 
         # Minimum threshold for taking detection into account
-        self.min_score_thresh = min_score_thresh
+        self.detection_threshold = 0.5
+
+        # Load TF session
+        self.sess = tf.Session(graph=self.detection_graph)
+
+    def load_graph(self):
+        graph = tf.Graph()
+        with graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(FROZEN_GRAPH_PATH, 'rb') as fid:
+                od_graph_def.ParseFromString(fid.read())
+                tf.import_graph_def(od_graph_def, name='')
+
+        # Also retrieve all tensor names
+        ops = graph.get_operations()
+        all_tensor_names = {output.name for op in ops for output in op.outputs}
+
+        return graph, all_tensor_names
+
+    def load_tensor(self, name):
+        # Retrieve tensor from graph
+        tensor_name = name + ':0'
+        if tensor_name in self.all_tensor_names:
+            return self.grpah.get_tensor_by_name(tensor_name)
+
+        return None
 
     def convert_class_id_to_traffic_light(self, class_id):
         if class_id == 1:
             return TrafficLight.GREEN
         if class_id == 2:
-            return TrafficLight.YELLOW
-        if class_id == 3:
             return TrafficLight.RED
+        if class_id == 3:
+            return TrafficLight.YELLOW
         else:
             return TrafficLight.UNKNOWN
 
@@ -38,48 +66,15 @@ class TLClassifier(object):
         """
 
         with self.detection_graph.as_default():
-            with tf.Session() as sess:
-                # Retrieve all tensors names from graph
-                ops = tf.get_default_graph().get_operations()
-                all_tensor_names = {output.name for op in ops for output in op.outputs}
+            # Run inference
+            num_detections, boxes, scores, classes = self.sess.run(
+                [self.num_detections, self.boxes, self.scores, self.classes],
+                feed_dict={self.image_tensor: np.expand_dims(image, 0)})
 
-                # Build a dict with only the tensors needed for detection results
-                tensor_dict = {}
-                for key in ['num_detections', 'detection_boxes', 'detection_scores', 'detection_classes', 'detection_masks']:
-                    tensor_name = key + ':0'
-                    if tensor_name in all_tensor_names:
-                        tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(tensor_name)
+            boxes = np.squeeze(boxes)
+            scores = np.squeeze(scores)
+            classes = np.squeeze(classes).astype(np.int32)
 
-                # Process tensors for single image inference
-                if 'detection_masks' in tensor_dict:
-                    detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
-                    detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
-                    # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
-                    real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
-                    detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
-                    detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
-                    detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(detection_masks, detection_boxes, image.shape[0], image.shape[1])
-                    detection_masks_reframed = tf.cast(tf.greater(detection_masks_reframed, 0.5), tf.uint8)
-                    # Follow the convention by adding back the batch dimension
-                    tensor_dict['detection_masks'] = tf.expand_dims(detection_masks_reframed, 0)
-
-                # Retrieve input image tensor for inference
-                image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
-
-                # Run inference
-                output_dict = sess.run(tensor_dict, feed_dict={image_tensor: np.expand_dims(image, 0)})
-
-                # All outputs are float32 numpy arrays, so convert types as appropriate
-                output_dict['num_detections'] = int(output_dict['num_detections'][0])
-                output_dict['detection_classes'] = output_dict[ 'detection_classes'][0].astype(np.uint8)
-                output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
-                output_dict['detection_scores'] = output_dict['detection_scores'][0]
-                if 'detection_masks' in output_dict:
-                    output_dict['detection_masks'] = output_dict['detection_masks'][0]
-
-                # Return the detection with highest score if above the min threshold
-                detected_class = None
-                if output_dict['num_detections'] > 0 and output_dict['detection_scores'][0] > self.min_score_thresh:
-                    detected_class = output_dict['detection_classes'][0]
-
-                return self.convert_class_id_to_traffic_light(detected_class)
+            # Return the detection with highest score if above the min threshold
+            detected_class = classes[0] if (num_detections > 0 and scores[0] > self.detection_threshold) else -1
+            return self.convert_class_id_to_traffic_light(detected_class)
--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -44,7 +44,7 @@ class TLDetector(object):
         self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
 
         self.bridge = CvBridge()
-        self.light_classifier = TLClassifier('../../../models/ssd_sim/frozen_inference_graph.pb')
+        self.light_classifier = TLClassifier()
         self.listener = tf.TransformListener()
 
         self.state = TrafficLight.UNKNOWN
